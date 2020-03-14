d1093f9be87c9e7559af39736e7a2667090ed1b3
aszpatowski <314211@uwr.edu.pl>
2020-03-14 17:11:55 +0100

    first_commit_2

--- test.py
+++ test.py
@@ -2,4 +2,8 @@ import pygame
 
 pygame.init()
 
-screen = pygame.display.set_mode((1920,1080))
+screen = pygame.display.set_mode((1920,1080),0,32)
+
+while True:
+    for event in pygame.event.get():
+        print(event)
